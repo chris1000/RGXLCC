4d7adac90b2fd7cba712bd6e178d324e53ccfb29
Chris Levine <chrislevine24@gmail.com>
2022-11-16 16:04:59 -0600

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,15 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown';
-
 import { remarkReadingTime } from './src/utils/frontmatter.js';
-
 import { SITE } from './src/config.mjs';
-
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-// https://astro.build/config
 export default defineConfig({
 	// Astro uses this full URL to generate your sitemap and canonical URLs in your final build
 	site: SITE.origin,
@@ -35,11 +29,9 @@ export default defineConfig({
 			serviceEntryPoint: '@astrojs/image/sharp',
 		}),
 		mdx(),
-
-		/* Disable this integration if you don't use Google Analytics (or other external script). */
 		partytown({
 			config: { forward: ['dataLayer.push'] },
-		}),
+		})
 	],
 
 	markdown: {
